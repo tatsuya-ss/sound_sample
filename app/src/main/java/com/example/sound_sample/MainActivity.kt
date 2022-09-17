0decf31b94fd79396cc0568a7c10cf01433d89ed
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2022-09-17 11:50:44 +0900

    一時停止機能追加

--- app/src/main/java/com/example/sound_sample/MainActivity.kt
+++ app/src/main/java/com/example/sound_sample/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.sound_sample.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +17,14 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.shining_star)
+
         binding.startButton.setOnClickListener {
-            val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.shining_star)
             mediaPlayer?.start()
+        }
+
+        binding.pauseButton.setOnClickListener {
+            mediaPlayer?.pause()
         }
     }
 }
