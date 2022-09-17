77e545ce414f83f3d8f813aff80fb721347ab6b0
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2022-09-17 13:20:00 +0900

    バックグラウンドで再生する処理実装

--- app/src/main/java/com/example/sound_sample/MainActivity.kt
+++ app/src/main/java/com/example/sound_sample/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.sound_sample
 
+import android.app.*
+import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import com.example.sound_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +22,87 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.shining_star)
+        val intent = Intent(this, MusicService::class.java)
 
         binding.startButton.setOnClickListener {
-            mediaPlayer?.start()
+            startService(intent)
         }
 
         binding.pauseButton.setOnClickListener {
-            mediaPlayer?.pause()
+            stopService(intent)
         }
     }
 }
+
+class MusicService : Service() {
+    private var mediaPlayer: MediaPlayer? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("Tatsuya", "onCreate: ")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val context = applicationContext
+
+        val channelId = "default"
+
+        val title = "アプリタイトル"
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val pendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
+            PendingIntent.getActivity(
+                this,
+                0,
+                notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+
+        val channel = NotificationChannel(
+            "default", title, NotificationManager.IMPORTANCE_DEFAULT
+        )
+
+        notificationManager.createNotificationChannel(channel)
+
+        val notification = Notification.Builder(context, channelId)
+            .setContentTitle(title) // android標準アイコンから
+            .setSmallIcon(android.R.drawable.ic_media_play)
+            .setContentText("MediaPlay")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setWhen(System.currentTimeMillis())
+            .build()
+
+        startForeground(1, notification)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.shining_star)
+        mediaPlayer?.start()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("debug", "onDestroy()")
+        audioStop()
+        // Service終了
+        stopSelf()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    private fun audioStop() {
+        // 再生終了
+        mediaPlayer?.stop()
+        // リセット
+        mediaPlayer?.reset()
+        // リソースの解放
+        mediaPlayer?.release()
+    }
+}
+
